653e23795cb8cdde783371eb7dfa89022fdfb0a0
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-07-25 13:42:10 -0300

    Atualizando Menu secundário

--- src/main/kotlin/Controller/MenuPrincipalController.kt
+++ src/main/kotlin/Controller/MenuPrincipalController.kt
@@ -6,6 +6,7 @@ import Utils.VerificaInputs
 import com.github.ajalt.mordant.rendering.TextColors
 import view.Menus
 import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
 
 class MenuPrincipalController(var carrinho: Carrinho,var produtoRepository: ProdutoRepository){
     fun menuPrincipalLanches(){
@@ -23,7 +24,10 @@ class MenuPrincipalController(var carrinho: Carrinho,var produtoRepository: Prod
                 VerificaInputs.t.println(TextColors.green("✅ Lanche Adicionado com sucesso\n"))
                 Menus.carrinho(carrinho)
                 break
-            }catch (erro: IllegalArgumentException){
+            }catch (erro: NumberFormatException){
+                println((TextColors.red("❌ Formato inválido,informe um valor numérico.")))
+            }
+            catch (erro: IllegalArgumentException){
                 println(erro.message)
             }
         }
@@ -43,7 +47,10 @@ class MenuPrincipalController(var carrinho: Carrinho,var produtoRepository: Prod
                 VerificaInputs.t.println(TextColors.green("✅ Bebida Adicionado com sucesso\n"))
                 Menus.carrinho(carrinho)
                 break
-            }catch (erro: IllegalArgumentException){
+            }catch (erro:NumberFormatException){
+                println((TextColors.red("❌ Formato inválido,informe um valor numérico.")))
+            }
+            catch (erro: IllegalArgumentException){
                 println(erro.message)
             }
         }
